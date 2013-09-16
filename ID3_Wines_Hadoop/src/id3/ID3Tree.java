24b3044964928e43c06a96a96efd3c656ea8642f
George Piskas <geopiskas@gmail.com>
2013-09-16 14:32:06 +0300

    Initial Commit

--- ID3_Wines_Hadoop/src/id3/ID3Tree.java
+++ ID3_Wines_Hadoop/src/id3/ID3Tree.java
@@ -1,6 +1,6 @@
 /*
 * Hadoop ID3
-* Copyright (C) 2013 George Piskas, George Oikonomidis
+* Copyright (C) 2013 George Piskas, George Economides
 *
 * This program is free software; you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
